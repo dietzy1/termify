efdab007d56157293383c78454c0e3e92df74e81
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2025-04-18 12:53:51 +0200

    chore: update and try to create recommend endpoint manually

--- go.mod
+++ go.mod
@@ -4,20 +4,22 @@ go 1.24.1
 
 require (
 	github.com/atotto/clipboard v0.1.4
-	github.com/charmbracelet/bubbles v0.20.0
+	github.com/charmbracelet/bubbles v0.21.0
 	github.com/charmbracelet/bubbletea v1.3.4
-	github.com/charmbracelet/lipgloss v1.0.0
+	github.com/charmbracelet/lipgloss v1.1.0
 	github.com/evertras/bubble-table v0.17.1
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c
 	github.com/zmb3/spotify/v2 v2.4.3
-	golang.org/x/oauth2 v0.27.0
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
+	golang.org/x/oauth2 v0.29.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/colorprofile v0.3.0 // indirect
 	github.com/charmbracelet/harmonica v0.2.0 // indirect
 	github.com/charmbracelet/x/ansi v0.8.0 // indirect
+	github.com/charmbracelet/x/cellbuf v0.0.13 // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
@@ -31,7 +33,8 @@ require (
 	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sahilm/fuzzy v0.1.1 // indirect
-	golang.org/x/sync v0.11.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 )
--- internal/state/search.go
+++ internal/state/search.go
@@ -56,3 +56,68 @@ func (s *SpotifyState) SearchEverything(query string) tea.Cmd {
 		}
 	}
 }
+
+// TODO: Work in progress
+func (s *SpotifyState) PlayRecommendTrack(trackID spotify.ID) tea.Cmd {
+
+	// Use the ID to perform a search similar to performing a recommendation so use the details from the track to recommend a new one
+	// We must use the client search function to do it
+
+	log.Printf("SpotifyState: Finding recommendation for track: %s", trackID)
+	if trackID == "" {
+		log.Printf("SpotifyState: Invalid track ID")
+	}
+
+	// Get track information
+	track, err := s.client.GetTrack(context.TODO(), trackID)
+	if err != nil {
+		log.Printf("SpotifyState: Error getting track info: %v", err)
+	}
+
+	// Extract artist name and track name to build a search query
+	var artistName string
+	if len(track.Artists) > 0 {
+		artistName = track.Artists[0].Name
+	}
+
+	// Create search query based on artist name or genre
+	searchQuery := artistName
+	if len(track.Album.Name) > 0 {
+		// Optionally include album info for better matching
+		searchQuery = fmt.Sprintf("artist:%s", artistName)
+	}
+
+	log.Printf("SpotifyState: Searching with query: %s", searchQuery)
+	results, err := s.client.Search(context.TODO(), searchQuery, spotify.SearchTypeTrack, spotify.Limit(50))
+	if err != nil {
+		log.Printf("SpotifyState: Error searching for similar tracks: %v", err)
+
+	}
+
+	// Check if we found any tracks
+	if len(results.Tracks.Tracks) == 0 {
+		log.Printf("SpotifyState: No similar tracks found")
+	}
+
+	log.Printf("SpotifyState: Found %d similar tracks", len(results.Tracks.Tracks))
+
+	// Filter out the original track and pick a random one from the results
+	var similarTracks []spotify.FullTrack
+	for _, t := range results.Tracks.Tracks {
+		if t.ID != trackID {
+			similarTracks = append(similarTracks, t)
+		}
+	}
+
+	if len(similarTracks) == 0 {
+		log.Printf("SpotifyState: No different tracks found")
+
+	}
+
+	// Select a track - for simplicity using the first different track found
+	// but you could randomize this or use some other selection criteria
+	selectedTrack := similarTracks[0]
+	log.Printf("SpotifyState: Playing recommended track: %s by %s", selectedTrack.Name, selectedTrack.Artists[0].Name)
+	// Play the selected track
+	return s.PlayTrack(selectedTrack.ID)
+}
