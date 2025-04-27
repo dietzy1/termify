07b31c4e747ae066532dfcf886ad54147eb06dd4
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2025-04-27 19:29:28 +0200

    feat: patch issue with spotify queue duplications still a better solution to be found

--- internal/state/queue.go
+++ internal/state/queue.go
@@ -25,6 +25,30 @@ func (s *SpotifyState) FetchQueue() tea.Cmd {
 		}
 		log.Printf("SpotifyState: Queue has %d items", len(queue.Items))
 
+		// Remove consecutive duplicate tracks if they appear 10 or more times in a row
+		if len(queue.Items) > 0 {
+			var deduped []spotify.FullTrack
+			current := queue.Items[0]
+			count := 1
+
+			for i := 1; i < len(queue.Items); i++ {
+				if queue.Items[i].ID == current.ID {
+					count++
+				} else {
+					if count < 5 {
+						deduped = append(deduped, current)
+					}
+					current = queue.Items[i]
+					count = 1
+				}
+			}
+			// Add the last track if it doesn't have too many duplicates
+			if count < 10 {
+				deduped = append(deduped, current)
+			}
+			queue.Items = deduped
+		}
+
 		s.mu.Lock()
 		s.queue = queue.Items
 		s.mu.Unlock()
@@ -34,16 +58,17 @@ func (s *SpotifyState) FetchQueue() tea.Cmd {
 }
 
 func (s *SpotifyState) AddToQueue(trackId spotify.ID) tea.Cmd {
-	return func() tea.Msg {
-		if err := s.client.QueueSong(context.TODO(), trackId); err != nil {
-			log.Printf("SpotifyState: Error adding track %s to queue: %v", trackId, err)
+
+	if err := s.client.QueueSong(context.TODO(), trackId); err != nil {
+		log.Printf("SpotifyState: Error adding track %s to queue: %v", trackId, err)
+		return func() tea.Msg {
 			return ErrorMsg{
 				Title:   fmt.Sprintf("Failed to Add Track %s to Queue", trackId),
 				Message: err.Error(),
 			}
 		}
-
-		log.Printf("SpotifyState: Successfully added track %s to queue, fetching updated queue...", trackId)
-		return s.FetchQueue()()
 	}
+
+	log.Printf("SpotifyState: Successfully added track %s to queue, fetching updated queue...", trackId)
+	return s.FetchQueue()
 }
